a0f4a2c525c18a1771db77f9b5561868c250a000
Agoston Toth <totha014@gmail.com>
2018-11-10 22:21:27 +0100

    Add GetFormattedBoard method to Board class

--- Beadando/Beadando/Board.cs
+++ Beadando/Beadando/Board.cs
@@ -9,12 +9,36 @@ namespace Beadando
     class Board
     {
         private int boardSize;
-        private Card[] board;        
+        private Card[] board;
+
+        public int BoardSize
+        {
+            get { return boardSize; }
+        }
+
 
         public Board(int boardSize)
         {
             this.boardSize = boardSize;
             board = new Card[this.boardSize];
+        }
+
+
+        public string GetFormattedBoard()
+        {
+            string formattedGameBoard = "";
+            for(int i = 0; i < board.Length; i++)
+            {
+                if(i + 1 % Math.Sqrt(boardSize) == 0)
+                {
+                    formattedGameBoard = board[i].Word + ";\n";
+                }else
+                {
+                    formattedGameBoard = board[i].Word + ";";
+                }                
+            }
+
+            return formattedGameBoard;
         }
 
         public void FillBoard(string[] words)
